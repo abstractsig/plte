a34c43afc9a38263aeac792dce4ddf90b9457ed9
abstractsig <>
2020-05-09 10:15:47 +1200

    change to socket open api

--- io_device/source/main.c
+++ io_device/source/main.c
@@ -12,6 +12,7 @@
 #define IMPLEMENT_IO_BOARD
 #include <io_device.h>
 
+
 int
 main (void) {
 	io_t *io = initialise_device_io ();
@@ -33,7 +34,7 @@ main (void) {
 			(first_run)? "":"\n",DBP_FIELD1,DEVICE_NAME,DBP_FIELD2,"startup"
 		);
 
-		io_socket_open (radio);
+		io_socket_open (radio,IO_SOCKET_OPEN_CONNECT);
 
 		while (1) {
 			io_wait_for_event (io);
